d79dceaee02856b05698405414fed3ebabee8800
unknown <jjune.heo@gmail.com>
2020-07-06 15:20:05 +0900

    Updates

---
+++ precache-manifest.2dcb92002ac3a6babd6d2e0602cf2ac8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81f91bc30eaa3bf41c51fae422ccd03b",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "281a8a4880850e9e9fba",
+    "url": "/movie_app_2020/static/css/main.be87f0a0.chunk.css"
+  },
+  {
+    "revision": "b4ad6d5796d4c9c3f3c1",
+    "url": "/movie_app_2020/static/js/2.3507762e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.3507762e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "281a8a4880850e9e9fba",
+    "url": "/movie_app_2020/static/js/main.546f34ea.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
